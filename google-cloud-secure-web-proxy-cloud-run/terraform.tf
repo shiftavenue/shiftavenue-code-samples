5b2812dd13cdda88f3cdf736515ab079d49c04b3
relusc <rene.schach@shiftavenue.com>
2024-12-15 11:02:02 +0100

    Remove beta provider

--- google-cloud-secure-web-proxy-cloud-run/terraform.tf
+++ google-cloud-secure-web-proxy-cloud-run/terraform.tf
@@ -4,17 +4,9 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 6.0"
     }
-    google-beta = {
-      source  = "hashicorp/google-beta"
-      version = "~> 6.0"
-    }
   }
 }
 
 provider "google" {
-
-}
-
-provider "google-beta" {
 
 }

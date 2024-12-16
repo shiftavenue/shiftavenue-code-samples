776473297a5362504f24e9d922b97fc2d8bc32dc
Rene Schach <rene.schach@shiftavenue.com>
2024-12-16 09:05:50 +0100

    Use latest network module version (#11)
    Signed-off-by: Rene Schach <rene.schach@shiftavenue.com>

--- google-cloud-secure-web-proxy-cloud-run/infra/network.tf
+++ google-cloud-secure-web-proxy-cloud-run/infra/network.tf
@@ -1,6 +1,6 @@
 module "vpc_egress" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 9.1"
+  version = "~> 10.0"
 
   project_id   = var.project_id
   network_name = "net-central-egress"
@@ -25,7 +25,7 @@ module "vpc_egress" {
 
 module "vpc_spoke" {
   source  = "terraform-google-modules/network/google"
-  version = "~> 9.1"
+  version = "~> 10.0"
 
   project_id   = var.project_id
   network_name = "net-spoke"
